ba3d878aae6c1d691bf331dbce4d132fdb49785b
Jmumo <johnmumo22@gmail.com>
2025-01-05 17:09:03 +0300

    Update spring security and update security definations

--- src/main/java/GiftsBackend/Config/OpenApiConfig.java
+++ src/main/java/GiftsBackend/Config/OpenApiConfig.java
@@ -1,23 +1,37 @@
 package GiftsBackend.Config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 
-@OpenAPIDefinition(info = @Info(title = "some title", description = "API for <project name> endpoints. For DataTransfer and Logs endpoints, please contact dev team.", version = "v0.1"),
+@OpenAPIDefinition(info = @Info(title = "Friends Gift App", description = "API for <project name> endpoints. For DataTransfer and Logs endpoints, please contact dev team.", version = "v0.1"),
         servers = {
                 @Server(
                         description = "localhost",
                         url = "http://localhost:8080"
                 ),
-//                @Server(
-//                        description = "dev server",
-//                        url = "https://<your-server-here>/"
-//                )
-        }
+//
+        },
+
+        security = @SecurityRequirement(
+                name = "GiftApp Security Scheme"
+        )
 )
 
+
+@SecurityScheme(
+        name = "GiftApp Security Scheme",
+        description = "Gift app Security description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class OpenApiConfig {
 
 }
